9585d570c972909964097c750127e7b7cea35204
Olzhas <olzhasayato@gmail.com>
2023-02-25 17:41:25 +0600

    basic http server && json parsing

---
+++ cmd/api/movies.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"greenlight_v3/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"Drama", "Romance", "War"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+
+}
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module greenlight_v3
+
+go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
